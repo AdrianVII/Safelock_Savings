565e0c2ebc7e004b5c02e3253af4a46a5fe4ef85
cimar <cimar3456@gmail.com>
2021-10-25 15:02:58 -0500

    Se agrego la visibilidad o la invisibilidad del boton de agregar

--- app/src/main/java/com/aa/safelocksaving/Main_Activity.java
+++ app/src/main/java/com/aa/safelocksaving/Main_Activity.java
@@ -2,6 +2,7 @@ package com.aa.safelocksaving;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -9,6 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationBarView;
 import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
@@ -18,6 +20,7 @@ public class Main_Activity extends AppCompatActivity {
     private Budgets_Fragments budgets_fragments = new Budgets_Fragments();
     private Reports_Fragments reports_fragments = new Reports_Fragments();
     private Settings_Fragments  settings_fragments =  new Settings_Fragments();
+    private FloatingActionButton add;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,13 +28,25 @@ public class Main_Activity extends AppCompatActivity {
         setContentView(R.layout.main_activity);
         chipNavigationBarMain = findViewById(R.id.chipNavigationBarMain);
         loadFragments(reminders_fragments);
-        //chipNavigationBarMain.setItemSelected(R.id.menuReminders, true);
+        add = findViewById(R.id.add);
         chipNavigationBarMain.setOnItemSelectedListener(item -> {
             switch (item) {
-                case R.id.menuReminders: loadFragments(reminders_fragments); break;
-                case R.id.menuBudgets: loadFragments(budgets_fragments); break;
-                case R.id.menuReport: loadFragments(reports_fragments);break;
-                case R.id.menuSettings: loadFragments(settings_fragments);break;
+                case R.id.menuReminders:
+                    loadFragments(reminders_fragments);
+                    add.setVisibility(View.VISIBLE);
+                    break;
+                case R.id.menuBudgets:
+                    loadFragments(budgets_fragments);
+                    add.setVisibility(View.VISIBLE);
+                break;
+                case R.id.menuReport:
+                    loadFragments(reports_fragments);
+                    add.setVisibility(View.INVISIBLE);
+                    break;
+                case R.id.menuSettings:
+                    loadFragments(settings_fragments);
+                    add.setVisibility(View.INVISIBLE);
+                    break;
             }
         });
     }
