edefca706586d4e8805a9f161604251db7ea06a3
cimar <cimar3456@gmail.com>
2021-10-25 14:39:23 -0500

    Creacion de los 4 vistas en fragmentos de la pantalla principal

--- app/src/main/java/com/aa/safelocksaving/Main_Activity.java
+++ app/src/main/java/com/aa/safelocksaving/Main_Activity.java
@@ -6,25 +6,37 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.navigation.NavigationBarView;
 import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
 public class Main_Activity extends AppCompatActivity {
     private ChipNavigationBar chipNavigationBarMain;
+    private Reminders_Fragments reminders_fragments = new Reminders_Fragments();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
         chipNavigationBarMain = findViewById(R.id.chipNavigationBarMain);
-        chipNavigationBarMain.setItemSelected(R.id.menuReminders, true);
+        loadFragments(reminders_fragments);
+        //chipNavigationBarMain.setItemSelected(R.id.menuReminders, true);
         chipNavigationBarMain.setOnItemSelectedListener(item -> {
             switch (item) {
-                case R.id.menuReminders: Toast.makeText(getBaseContext(), "Recordatorios", Toast.LENGTH_SHORT).show(); break;
+                case R.id.menuReminders: loadFragments(reminders_fragments); break;
                 case R.id.menuBudgets: Toast.makeText(getBaseContext(), "Presupuestos", Toast.LENGTH_SHORT).show(); break;
                 case R.id.menuReport: Toast.makeText(getBaseContext(), "Reportes", Toast.LENGTH_SHORT).show(); break;
                 case R.id.menuSettings: Toast.makeText(getBaseContext(), "Ajustes", Toast.LENGTH_SHORT).show(); break;
             }
         });
     }
+
+    public void loadFragments(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment, fragment);
+        transaction.commit();
+    }
+
 }
---
+++ app/src/main/java/com/aa/safelocksaving/Reminders_Fragments.java
@@ -0,0 +1,32 @@
+package com.aa.safelocksaving;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Reminders_Fragments extends Fragment {
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+        return inflater.inflate(R.layout.reminders_fragments, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    }
+}
