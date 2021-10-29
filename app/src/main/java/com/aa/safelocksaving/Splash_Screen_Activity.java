bc5a2f5eb12bdd060d328da97558ce3e4f19ad8c
cimar <cimar3456@gmail.com>
2021-10-28 21:33:27 -0500

    Se corrigio el Splash Screen

--- app/src/main/java/com/aa/safelocksaving/Splash_Screen_Activity.java
+++ app/src/main/java/com/aa/safelocksaving/Splash_Screen_Activity.java
@@ -3,12 +3,14 @@ package com.aa.safelocksaving;
 import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.util.Pair;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -17,12 +19,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.aa.safelocksaving.DAO.DAOConfigurationData;
 
 public class Splash_Screen_Activity extends AppCompatActivity {
-
+    private MyCountDownTimer countDownTimer;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         new DAOConfigurationData(this).setLanguage();
         setContentView(R.layout.splash_screen_activity);
+        countDownTimer = new MyCountDownTimer(2000, 1000);
         Animation animation1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
         Animation animation2 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
 
@@ -34,12 +37,23 @@ public class Splash_Screen_Activity extends AppCompatActivity {
 
         new Handler().postDelayed(() -> {
             Intent intent = new Intent(getBaseContext(), Start_Activity.class);
-            Pair[] pairs = new Pair[2];
-            pairs[0] = new Pair<View, String>(imageView, "imageViewTransition");
-            pairs[1] = new Pair<View, String>(textView, "textViewTransition");
-            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(this, pairs);
-            startActivity(intent, options.toBundle());
-            //finish();
-        }, 4000);
+            startActivity(intent);
+            countDownTimer.start();
+        }, 2000);
     }
+
+    class MyCountDownTimer extends CountDownTimer {
+        public MyCountDownTimer(long startTime, long interval) { super(startTime, interval); }
+
+        @Override
+        public void onTick(long millisUntilFinished) {
+
+        }
+
+        @Override
+        public void onFinish() {
+            finish();
+        }
+    }
+
 }
